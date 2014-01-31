85f67fd7b047ff008866c8bfb0c79d82ce7a7e79
Divyavrat <Divyavrat@gmail.com>
2014-01-31 20:20:07 +0530

    Initial codes.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ dwarf-os
 ========
 
 A 512 byte OS that can run simple programs and edit text or binary code.
+Just use any hex editor such as HxD to write dwarf.com to the starting sector 0
+of the drive.
---
+++ build.bat
@@ -0,0 +1,11 @@
+fasm dwarf.asm dwarf.com
+fasm help.asm help.com
+fasm shut.asm shut.com
+fasm restart.asm restart.com
+fasm fhlt.asm fhlt.com
+fasm text.asm text.com
+fasm code.asm code.com
+fasm snake.asm snake.com
+copy /b dwarf.com+text.com+code.com+help.com+shut.com+restart.com+fhlt.com+help.com+snake.com dwarf.img
+pause
+OSDev.lnk
