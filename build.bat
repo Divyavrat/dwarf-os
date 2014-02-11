cf3093a7361981c910cbd65cdf0bf35605bac345
Divyavrat <Divyavrat@gmail.com>
2014-02-11 18:46:29 +0530

    Added a dwarfapp.

--- build.bat
+++ build.bat
@@ -6,10 +6,11 @@ fasm fhlt.asm fhlt.com
 fasm text.asm text.com
 fasm code.asm code.com
 fasm watch.asm watch.com
+fasm dwarfapp.asm dwarfapp.com
 fasm clock.asm clock.com
 fasm snake.asm snake.com
 fasm play.asm play.com
 fasm test.asm test.com
-copy /b dwarf.com+text.com+code.com+help.com+shut.com+restart.com+fhlt.com+clock.com+watch.com+snake.com+play.com+test.com dwarf.img
+copy /b dwarf.com+text.com+code.com+help.com+shut.com+restart.com+fhlt.com+clock.com+dwarfapp.com+watch.com+snake.com+play.com+test.com dwarf.img
 pause
 OSDev.lnk
